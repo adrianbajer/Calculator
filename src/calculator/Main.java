e836cdef9d0ddee115cf81a0ec985862c38c7c5c
Adrian Bajer <adrianbajer@o2.pl>
2019-05-20 21:30:29 +0200

    naming_change

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kalkulator;
+package calculator;
 
 /**
  *
@@ -14,14 +14,14 @@ import java.util.Scanner;
 
 
 
-public class Kalkulator {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double wynik = 0;
+        double result = 0;
         
         Scanner sc = new Scanner(System.in);
             System.out.println("Podaj pierwszą liczbę: ");
@@ -30,33 +30,33 @@ public class Kalkulator {
                     System.out.println("Błąd! Wprowadź poprawną liczbę.");
                     sc.next();
                 }
-                    double a = sc.nextDouble();
+                    double firstUserNumber = sc.nextDouble();
             System.out.println("Podaj drugą liczbę: ");
                 while (!sc.hasNextDouble())
                 {
                     System.out.println("Błąd! Wprowadź poprawną liczbę.");
                     sc.next();
                 }
-                    double b = sc.nextDouble();
+                    double secondUserNumber = sc.nextDouble();
                     sc.nextLine();
             System.out.println("Podaj znak działania, które chcesz wykonać (+ - * / ^): ");
                 String operationSymbol = sc.nextLine();
 
         switch (operationSymbol) {
             case "+":
-                wynik = a+b;
+                result = firstUserNumber+secondUserNumber;
                 break;
             case "-":
-                wynik = a-b;
+                result = firstUserNumber-secondUserNumber;
                 break;
             case "*":
-                wynik = a*b;
+                result = firstUserNumber*secondUserNumber;
                 break;
             case "/":
-                wynik = a/b;
+                result = firstUserNumber/secondUserNumber;
                 break;
             case "^":
-                wynik = Math.pow(a, b);
+                result = Math.pow(firstUserNumber, secondUserNumber);
                 break;
             default:
                 System.out.println("Podałeś zły znak działania!");
@@ -64,7 +64,7 @@ public class Kalkulator {
                 System.exit(0);
         }
         
-        System.out.println(wynik);
+        System.out.println(result);
                 
                 
         
